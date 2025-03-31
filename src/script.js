0ed536c27f95f501ac679391631e4a4c3ac392aa
Jmsteele01 <Jmsteele01@hotmail.com>
2025-03-31 16:55:34 -0600

    basic js intigration

--- src/script.js
+++ src/script.js
@@ -1,1 +1,39 @@
-let answer1 = document.getElementById("question 2")
+let points = [0, 0, 0, 0, 0];
+
+let question2 = document.getElementsByName("Question 2");
+
+let answer2 = "";
+
+console.log(question2[0].value);
+
+let x = 0;
+do{
+    if(question2[x].checked)
+    {
+        answer2 = question2[x].value;
+        points[x]++;
+        break;
+    }
+x++;
+}while(x < question2.length);
+
+console.log(answer2);
+
+console.log(document.getElementById("q3"));
+
+function test()
+{
+    console.log("foobar");
+
+    let x = 0;
+do{
+    if(question2[x].checked)
+    {
+        answer2 = question2[x].value;
+        points[x]++;
+        break;
+    }
+x++;
+}while(x < question2.length);
+console.log(answer2);
+};
