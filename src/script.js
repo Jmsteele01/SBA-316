d29e526858fdf9e4fb18f23925ff47777f8a6525
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-01 14:44:25 -0600

    basic question calc

--- src/script.js
+++ src/script.js
@@ -1,39 +1,35 @@
-let points = [0, 0, 0, 0, 0];
-
-let question2 = document.getElementsByName("Question 2");
-
-let answer2 = "";
-
-console.log(question2[0].value);
-
-let x = 0;
-do{
-    if(question2[x].checked)
-    {
-        answer2 = question2[x].value;
-        points[x]++;
-        break;
-    }
-x++;
-}while(x < question2.length);
-
-console.log(answer2);
-
-console.log(document.getElementById("q3"));
-
-function test()
+let points = [0, 1, 2, 3, 4];
+function submission()
 {
-    console.log("foobar");
+    let question1 = document.getElementsByName("Question 1");
+    let question2 = document.getElementsByName("Question 2");
+    checkQuestion(question1, false)
+    console.log(points);
+// let fColor = document.getElementById(q3)
+// console.log(fColor);
 
-    let x = 0;
-do{
-    if(question2[x].checked)
-    {
-        answer2 = question2[x].value;
-        points[x]++;
-        break;
-    }
-x++;
-}while(x < question2.length);
-console.log(answer2);
+//resets points
+points = [0, 1, 2, 3, 4];
 };
+
+function checkQuestion(question, isRadio)
+{
+    let answer = "";
+    let x = 0;
+    do{
+        if(question[x].checked)
+        {
+            answer = question[x].value;
+            points[x] += 100;
+            console.log(question[x] +": "+ points[x]);
+            if(isRadio)
+                return answer; 
+        }
+    x++;
+    }while(x < question.length);
+};
+
+function getResults(finalScores)
+{
+
+}
