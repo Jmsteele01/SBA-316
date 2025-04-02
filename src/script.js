5c3354916cff0a416e4447cc56691af8c30e3a07
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-01 19:58:30 -0600

    Styling

--- src/script.js
+++ src/script.js
@@ -1,5 +1,3 @@
-let points = [0, 0, 0];
-
 const opening = document.body.querySelector(".opening");
 let title = document.createElement("h1");
 let description = document.createElement("p");
@@ -7,8 +5,8 @@ let description = document.createElement("p");
 opening.appendChild(title);
 opening.appendChild(description);
 
-title.textContent = "Pokemon Quiz!"
-description.textContent = "Take the quiz here!"
+title.textContent = "Pokemon Starter Quiz!"
+description.textContent = "Which Kanto starter are you?"
 
 // opening.textContent = "Waaaa";
 
@@ -49,39 +47,40 @@ description.textContent = "Take the quiz here!"
 // createListItems(ordered, 3);
 // createListItems(unordered, 5);
 
+document.getElementById("submit_btn").addEventListener("click", submission);
+
 function submission()
 {
+    let points = [0, 0, 0];
     let question1 = document.getElementsByName("Question 1");
     let question2 = document.getElementsByName("Question 2");
-    checkQuestion(question1, false);
+    checkQuestion(question1, points);
     console.log(points);
-    checkQuestion(question2, true);
+    checkQuestion(question2, points);
     let fColor = document.getElementById("q3");
-    checkColor(fColor.value);
+    checkColor(fColor.value, points);
     console.log(getResults(points));
 
     console.log(points);
 
     //resets points
-    points = [0, 0, 0];
+   //points = [0, 0, 0];
 
     document.body.style.backgroundColor = fColor.value;
 };
 
-function checkQuestion(question)
-{
-    let answer = "";
-    let x = 0;
+function checkQuestion(question, score)
+{    let x = 0;
     do{
         if(question[x].checked)
         {
             switch (question[x].value)
             {
-                case "b": points[0]+=150;
+                case "b": score[0]+=150;
                 break;
-                case "c": points[1]+=150;
+                case "c": score[1]+=150;
                 break;
-                case "s": points[2]+=150;
+                case "s": score[2]+=150;
                 break;
                 default: console.log("No valid input");
             }
@@ -112,7 +111,7 @@ function getResults(finalScores)
     }
 }
 
-function checkColor(colorSelect)
+function checkColor(colorSelect, score)
 {
     let r = parseInt(colorSelect.substring(1, 3), 16);
     let g = parseInt(colorSelect.substring(3, 5), 16);
@@ -120,7 +119,7 @@ function checkColor(colorSelect)
 
     console.log(r);
 
-    points[0] += g;
-    points[1] += r;
-    points[2] += b;
+    score[0] += g;
+    score[1] += r;
+    score[2] += b;
 }
